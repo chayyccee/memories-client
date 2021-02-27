9bf25a822ba7d95f98cc53fa77cc8729551f7090
chayyccee <ekechinedu488@gmail.com>
2021-02-27 07:33:23 +0100

    added delete functionality

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 
+import { deletePost } from '../../../actions/posts';
+
 import useStyles from './styles';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -38,7 +44,7 @@ const Post = ({ post, setCurrentId }) => {
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -3,6 +3,8 @@ export default (posts = [], action) => {
     // so i used the switch and case instead
 
     switch (action.type) {
+        case 'DELETE':
+            return posts.filter((post) => post._id !== action.payload); // keep all the post except the one where post id is not same as the action.payload
         case 'UPDATE':
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case 'FETCH_ALL':
