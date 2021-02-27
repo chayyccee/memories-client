15f412e241d07b097d3111f9d8d7b5d7a1f55d2c
chayyccee <ekechinedu488@gmail.com>
2021-02-27 08:19:45 +0100

    added like functionality

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -6,7 +6,12 @@ export default (posts = [], action) => {
         case 'DELETE':
             return posts.filter((post) => post._id !== action.payload); // keep all the post except the one where post id is not same as the action.payload
         case 'UPDATE':
+        case 'LIKE':
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+// since likePost is very similar to updatePost, we could have something like this below
+        // case 'LIKE':
+            // return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+// instead, add like case to update case as done above.
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
